3cfc9cbb20c73a6ea86137ef1e4c239a1406ae45
Moeein Aali <moeeeinaali@gmail.com>
2024-08-25 22:40:19 +0330

    Add Basic-Rules Solutions

---
+++ Assignments/1 - Basic rules/کار با آبجکت/src/index.ts
@@ -0,0 +1,18 @@
+interface User {
+    name: string,
+    username: string,
+    email: string,
+    phoneNumber: number,
+    password: string,
+    isMale: boolean
+}
+const user: User = {
+    name: "moeein",
+    username: "moeein",
+    email: "moeeeeinaali@gmail.com",
+    phoneNumber: 91234566789,
+    password: "12345678m",
+    isMale: true
+}
+
+export { User, user };
